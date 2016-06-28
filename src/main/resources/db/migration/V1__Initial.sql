861b2fe6e1e5c7a91418af424e70b5fcb4ffc946
Andy Wilkinson <awilkinson@pivotal.io>
2016-06-28 17:24:30 +0100

    Add support for projects managed in Jira

--- src/main/resources/db/migration/V1__Initial.sql
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -5,6 +5,12 @@ create table git_hub_project (
 	repo character varying(255) not null
 );
 
+create table jira_project (
+	id serial not null primary key,
+	name character varying(255) not null,
+	key character varying(255) not null,
+);
+
 create table release (
 	id serial not null primary key,
 	project character varying(255) not null,
@@ -16,3 +22,6 @@ insert into git_hub_project(name, owner, repo) values('Spring Boot', 'spring-pro
 insert into git_hub_project(name, owner, repo) values('Spring REST Docs', 'spring-projects', 'spring-restdocs');
 insert into git_hub_project(name, owner, repo) values('Spring Security', 'spring-projects', 'spring-security');
 insert into git_hub_project(name, owner, repo) values('Spring Session', 'spring-projects', 'spring-session');
+
+insert into jira_project(name, key) values('Spring Framework', 'SPR');
+insert into jira_project(name, key) values('Spring Integration', 'INT');
