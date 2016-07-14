953478fd2ac8c693213af67d6a961ccece718a1b
Andy Wilkinson <awilkinson@pivotal.io>
2016-07-14 14:49:18 +0100

    Add convention-based ResponseCreator for use with MockRestServiceServer

---
+++ src/test/java/io/spring/calendar/test/TestMethodResponseCreator.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.calendar.test;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.test.web.client.ResponseCreator;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
+
+/**
+ * A {@link ResponseCreator} that returns a successful response with the body loaded from
+ * a file named <code>{testClass.getSimpleName()}-{testMethod.getName()}.json}</code> in
+ * the test class's package.
+ *
+ * @author Andy Wilkinson
+ */
+public class TestMethodResponseCreator implements ResponseCreator {
+
+	private Class<?> testClass;
+
+	private Method testMethod;
+
+	@Override
+	public ClientHttpResponse createResponse(ClientHttpRequest request)
+			throws IOException {
+		return MockRestResponseCreators.withSuccess().body(getCurrentResource())
+				.contentType(MediaType.APPLICATION_JSON).createResponse(request);
+	}
+
+	void setTest(Class<?> testClass, Method testMethod) {
+		this.testClass = testClass;
+		this.testMethod = testMethod;
+	}
+
+	private Resource getCurrentResource() {
+		String resourceName = this.testClass.getSimpleName() + "-"
+				+ this.testMethod.getName() + ".json";
+		return new UrlResource(this.testClass.getResource(resourceName));
+	}
+
+}
---
+++ src/test/java/io/spring/calendar/test/TestMethodResponseTestExecutionListener.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.calendar.test;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.TestExecutionListener;
+import org.springframework.test.context.support.AbstractTestExecutionListener;
+
+/**
+ * A {@link TestExecutionListener} that enables the usage of
+ * {@link TestMethodResponseCreator}.
+ *
+ * @author Andy Wilkinson
+ */
+public final class TestMethodResponseTestExecutionListener
+		extends AbstractTestExecutionListener {
+
+	private final TestMethodResponseCreator testMethodResponseCreator = new TestMethodResponseCreator();
+
+	@Override
+	public void beforeTestClass(TestContext testContext) throws Exception {
+		((ConfigurableBeanFactory) testContext.getApplicationContext()
+				.getAutowireCapableBeanFactory()).registerSingleton("jsonResponseCreator",
+						this.testMethodResponseCreator);
+	}
+
+	@Override
+	public void beforeTestMethod(TestContext testContext) throws Exception {
+		this.testMethodResponseCreator.setTest(testContext.getTestClass(),
+				testContext.getTestMethod());
+	}
+
+}
