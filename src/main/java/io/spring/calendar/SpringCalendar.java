7522b459a78ecf58b578bb3c923c91aa931dc729
Andy Wilkinson <awilkinson@pivotal.io>
2020-04-01 19:14:29 +0100

    Configure HTTP client timeouts to reduce risk of a hung HTTP request

--- src/main/java/io/spring/calendar/SpringCalendar.java
+++ src/main/java/io/spring/calendar/SpringCalendar.java
@@ -16,8 +16,15 @@
 
 package io.spring.calendar;
 
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -31,6 +38,18 @@ public class SpringCalendar {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCalendar.class, args);
+	}
+
+	@Bean
+	public RestTemplateCustomizer httpClientTimeoutRestTemplateCustomizer() {
+		return (restTemplate) -> {
+			RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(30000)
+					.setConnectionRequestTimeout(30000).setSocketTimeout(30000).build();
+			CloseableHttpClient httpClient = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
+			HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
+					httpClient);
+			restTemplate.setRequestFactory(requestFactory);
+		};
 	}
 
 }
