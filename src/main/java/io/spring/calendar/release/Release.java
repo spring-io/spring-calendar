ce85971db6178fb8694927b9ccfb11513e52cd8b
Andy Wilkinson <wilkinsona@vmware.com>
2023-11-16 13:53:06 +0000

    Remove redundant semi-colons after enum declarations

--- src/main/java/io/spring/calendar/release/Release.java
+++ src/main/java/io/spring/calendar/release/Release.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2019 the original author or authors.
+ * Copyright 2016-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -46,7 +46,7 @@ public class Release {
 		 */
 		UNKNOWN
 
-	};
+	}
 
 	private final String project;
 
